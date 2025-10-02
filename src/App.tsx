6047124e72c7f14a656913b79ea39212c34dd44b
nathomp3 <nathomp3@ncsu.edu>
2025-10-02 02:40:36 -0400

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div>
+        <h2>AWC</h2>
+        <ul>
+          <li><a href="https://tokkumu.com/AWC2025/">AWC 2025</a></li>
+        </ul>
+        <h2>AWCC</h2>
+        <ul>
+          <li><a href="https://tokkumu.com/AWCC-Halloween-2025/">AWCC Haunted Adventure '25</a></li>
+        </ul>
+      </div>
+    </>
+  )
+}
+
+export default App
